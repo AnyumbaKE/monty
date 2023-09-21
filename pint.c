27a01d20a50228a2ffe3645557b150ac841d012d
AnyumbaKE <esanyumba@gmail.com>
2023-09-21 09:57:32 -0700

    pint

---
+++ pint.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * pint - print the value at the top of the stack
+ * @stack: stack given by main in start.c
+ * @line_number: amount of lines
+ *
+ * Return: void
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (!stack || !(*stack))
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+
+	}
+	printf("%d\n", (*stack)->n);
+}
