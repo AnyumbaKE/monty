21eaea0062a7a8deb44476e5eb6565c3f9429cf1
AnyumbaKE <esanyumba@gmail.com>
2023-09-21 11:01:59 -0700

    nop.c

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop -  does nothing
+ * @stack: doesnt matter
+ * @line_number : for nothing
+ *
+ * Return: NOTHING
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_cnt;
+}
