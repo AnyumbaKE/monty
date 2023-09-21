e553f1caf26e1cfaeb290d0ab7fedc1af36fb17c
AnyumbaKE <esanyumba@gmail.com>
2023-09-21 11:07:57 -0700

    builtins.c

---
+++ builtins.c
@@ -0,0 +1,39 @@
+#include "./monty.h"
+
+/**
+ * opcode - function in charge of running builtins
+ * @stack: stack given by main
+ * @str: string to compare
+ * @line_number: amount of lines
+ *
+ * Return: nothing
+ */
+void opcode(stack_t **stack, char *str, unsigned int line_number)
+{
+	int i = 0;
+
+	instruction_t op[] = INSTRUCTIONS;
+
+	if (!strcmp(str, "stack"))
+	{
+		global.data_struct = 1;
+		return;
+	}
+	if (!strcmp(str, "queue"))
+	{
+		global.data_struct = 0;
+		return;
+	}
+
+	while (op[i].opcode)
+	{
+		if (strcmp(op[i].opcode, str) == 0)
+		{
+			op[i].f(stack, line_number);
+			return;
+		}
+		i++;
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, str);
+	exit(EXIT_FAILURE);
+}
