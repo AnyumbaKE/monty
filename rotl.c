535d7242b45ceeb606c3ccebbb38cf762c3278ed
AnyumbaKE <esanyumba@gmail.com>
2023-09-21 11:39:03 -0700

    rotl.c

---
+++ rotl.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+* rotl - rotates the first element of the stack
+* @stack: stack head
+* @line_number: line count
+*
+* Return: void
+*/
+void rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t *left;
+	stack_t *right;
+
+	(void) line_count;
+	if (!stack || !*stack || !(*stack)->next)
+		return;
+
+	left = right = *stack;
+
+	while (right->next)
+		right = right->next;
+	right->next = left;
+	left->prev = right;
+	*stack = left->next;
+	(*stack)->prev->next = NULL;
+	(*stack)->prev = NULL;
+}
