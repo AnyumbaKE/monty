ea846c3e2193354d15d32d84dd7185b90af2b889
AnyumbaKE <esanyumba@gmail.com>
2023-09-21 11:25:03 -0700

    mod.c

---
+++ mod.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * mod - computes the remainder of the division
+ * @stack: stack given by main
+ * @line_number: line counter
+ *
+ * Return: void
+ */
+void mod(stack_t **stack, unsigned int line_number)
+{
+	int result;
+
+	if (!stack || !*stack || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+		return;
+	}
+	if (((*stack)->n) == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+		return;
+	}
+
+	result = ((*stack)->next->n) % ((*stack)->n);
+	pop(stack, line_cnt);
+	(*stack)->n = result;
+}
