a2155420f16e5bd9dddab4c63d328356dec9d44e
AnyumbaKE <esanyumba@gmail.com>
2023-09-21 09:45:12 -0700

    pall

---
+++ pall.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * pall - prints the stack
+ * @stack: stack given by main in start.c
+ * @line_number: amount of lines
+ *
+ * Return: void
+ */
+void pall(stack_t **stack, unsigned int line_number __attribute__((unused)))
+{
+	print_stack(*stack);
+}
