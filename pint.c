cce1a36841a2ebbb6a569695d95952f622438859
MARGARET WANJIRU <133248970+QueenMaggie@users.noreply.github.com>
2023-09-22 13:51:14 +0300

    Update pint.c

--- pint.c
+++ pint.c
@@ -7,7 +7,6 @@
  * pint - print the value at the top of the stack
  * @stack: stack given by main in start.c
  * @line_cnt: amount of lines
- *
  * Return: void
  */
 void pint(stack_t **stack, unsigned int line_cnt)
