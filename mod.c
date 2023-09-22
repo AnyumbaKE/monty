e1d38edf7fd293beaeef6a0ae11eabbbd8db7104
MARGARET WANJIRU <133248970+QueenMaggie@users.noreply.github.com>
2023-09-22 13:46:00 +0300

    Update mod.c

--- mod.c
+++ mod.c
@@ -8,7 +8,6 @@
  * mod - computes the remainder of the division
  * @stack: stack given by main
  * @line_cnt: line counter
- *
  * Return: void
  */
 void mod(stack_t **stack, unsigned int line_cnt)
