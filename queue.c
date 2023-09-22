57a3f0b8198caa8a100a59361a96036cde1ce0b9
MARGARET WANJIRU <133248970+QueenMaggie@users.noreply.github.com>
2023-09-22 13:54:10 +0300

    Update queue.c

--- queue.c
+++ queue.c
@@ -6,7 +6,6 @@
  * queue_node - adds a node to a stack_t stack in queue_node
  * @stack: stack head
  * @n: number of the node
- *
  * Return: newly created node, if memory allocation fails, the function will
  * return NULL.
  */
