45e6d4ffcacbe3005862fcf9bdb6d85327be7cc3
MARGARET WANJIRU <133248970+QueenMaggie@users.noreply.github.com>
2023-09-22 13:45:30 +0300

    Update handler.c

--- handler.c
+++ handler.c
@@ -61,7 +61,6 @@ int main(int argc, char **argv)
 /**
  * file_error - prints file error message and exits
  * @argv: argv given by main()
- *
  * Desc: print msg if  not possible to open the file
  * Return: nothing
  */
@@ -76,7 +75,6 @@ void file_error(char *argv)
  *
  * Desc: if user does not give any file or more than
  * one argument to your program
- *
  * Return: nothing
  */
 void error_usage(void)
