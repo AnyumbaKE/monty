fca301f95455de9524325f0fd908ad93b8dacec0
MARGARET WANJIRU <133248970+QueenMaggie@users.noreply.github.com>
2023-09-22 13:41:35 +0300

    Update add.c

--- add.c
+++ add.c
@@ -8,7 +8,6 @@
  * _add -  adds the first two nodes of the stack
  * @stack: stack given by main
  * @line_cnt: line counter
- *
  * Return: void
  */
 void _add(stack_t **stack, unsigned int line_cnt)
